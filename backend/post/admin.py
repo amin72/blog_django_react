f5385e80e5d4b53f43577b036db1b0b8be438641
Amin <asaa47721@gmail.com>
2020-05-14 03:40:09 +0430

    define TagAdmin. set attributes for PostAdmin

--- backend/post/admin.py
+++ backend/post/admin.py
@@ -4,7 +4,15 @@ from .models import Post, Tag
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
-    
+    list_display = ['title', 'author', 'status']
+    search_fields = ['title', 'content', 'author__username']
+    list_editable = ['status']
+    readonly_fields = ['total_likes', 'total_comments', 'slug']
 
-admin.site.register(Tag)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    readonly_fields = ['name']
