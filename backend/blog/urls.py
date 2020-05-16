bba917d920ed64266293d5fa537e9a4dbf484e78
Amin <asaa47721@gmail.com>
2020-05-16 10:13:56 +0430

    add MEDIA settings and url

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     # post urls
     path('v1/posts/', include('post.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
