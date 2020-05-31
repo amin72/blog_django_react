0ed39d0cc8279c3639e34698db20c4074ce745e5
Amin <asaa47721@gmail.com>
2020-05-31 18:14:35 +0430

    add jwt authentication

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -131,3 +131,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = 'account.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,13 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # post urls
-    path('v1/posts/', include('post.urls')),
+    path('api/v1/posts/', include('post.urls')),
+
+    # jwt: obtain token
+    path('api/v1/token/',
+        jwt_views.TokenObtainPairView.as_view(),
+        name='token_obtain_pair'),
+    
+    # jwt: refresh token
+    path('api/v1/token/refresh/',
+        jwt_views.TokenRefreshView.as_view(),
+        name='token_refresh'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
