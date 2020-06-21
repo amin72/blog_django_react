1882a4aa2be979ea27a8d23bf759b46618b4cad9
Amin <asaa47721@gmail.com>
2020-06-21 20:15:14 +0430

    define new types

--- frontend/react_web_app/src/context/types.js
+++ frontend/react_web_app/src/context/types.js
@@ -1,6 +1,10 @@
 // posts
-export const ADD_POST = 'ADD_POST'
-export const DELETE_POST = 'DELETE_POST'
-export const EDIT_POST = 'EDIT_POST'
+export const ADD_POST = 'ADD_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
 
 // users
+export const CREATE_USER = 'CREATE_USER';
+export const GET_TOKEN = 'GET_TOKEN';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const USER_DETAIL = 'USER_DETAIL';
